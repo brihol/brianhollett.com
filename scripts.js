c284015b9ca87475fc47d904af95f5aee2969b5a
brihol <brianhollett@gmail.com>
2024-06-19 14:01:18 -0700

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,218 @@
+/**
+ * Dark Mode Toggle 1.0.2
+ * Copyright 2023 Timothy Ricks
+ * Released under the MIT License
+ * Released on: November 28, 2023
+ */
+
+/**
+ * Additional scripts and modifications
+ * Copyright (c) 2024 Brian Hollett
+ * Released under the MIT License
+ */
+
+function colorModeToggle() {
+  function attr(defaultVal, attrVal) {
+    const defaultValType = typeof defaultVal;
+    if (typeof attrVal !== "string" || attrVal.trim() === "") return defaultVal;
+    if (attrVal === "true" && defaultValType === "boolean") return true;
+    if (attrVal === "false" && defaultValType === "boolean") return false;
+    if (isNaN(attrVal) && defaultValType === "string") return attrVal;
+    if (!isNaN(attrVal) && defaultValType === "number") return +attrVal;
+    return defaultVal;
+  }
+
+  const htmlElement = document.documentElement;
+  const computed = getComputedStyle(htmlElement);
+  let toggleEl;
+  let togglePressed = "false";
+  const darkIcon = document.querySelector(".dark-icon");
+  const liteIcon = document.querySelector(".lite-icon");
+
+  function setIconVisibility(isDarkMode) {
+    darkIcon.style.display = isDarkMode ? "block" : "none";
+    liteIcon.style.display = isDarkMode ? "none" : "block";
+  }
+
+  const scriptTag = document.querySelector("[tr-color-vars]");
+  if (!scriptTag) {
+    console.warn("Script tag with tr-color-vars attribute not found");
+    return;
+  }
+
+  let colorModeDuration = attr(0.5, scriptTag.getAttribute("duration"));
+  let colorModeEase = attr("power1.out", scriptTag.getAttribute("ease"));
+
+  const cssVariables = scriptTag.getAttribute("tr-color-vars");
+  if (!cssVariables.length) {
+    console.warn("Value of tr-color-vars attribute not found");
+    return;
+  }
+
+  let lightColors = {};
+  let darkColors = {};
+  cssVariables.split(",").forEach(function (item) {
+    let lightValue = computed.getPropertyValue(`--color--${item}`);
+    let darkValue = computed.getPropertyValue(`--dark--${item}`);
+    if (lightValue.length) {
+      if (!darkValue.length) darkValue = lightValue;
+      lightColors[`--color--${item}`] = lightValue;
+      darkColors[`--color--${item}`] = darkValue;
+    }
+  });
+
+  if (!Object.keys(lightColors).length) {
+    console.warn("No variables found matching tr-color-vars attribute value");
+    return;
+  }
+
+  function setColors(colorObject, animate) {
+    if (typeof gsap !== "undefined" && animate) {
+      gsap.to(htmlElement, {
+        ...colorObject,
+        duration: colorModeDuration,
+        ease: colorModeEase
+      });
+    } else {
+      Object.keys(colorObject).forEach(function (key) {
+        htmlElement.style.setProperty(key, colorObject[key]);
+      });
+    }
+  }
+
+  function goDark(dark, animate) {
+    const isDarkMode = dark;
+    if (dark) {
+      localStorage.setItem("dark-mode", "true");
+      htmlElement.classList.add("dark-mode");
+      setColors(darkColors, animate);
+      togglePressed = "true";
+    } else {
+      localStorage.setItem("dark-mode", "false");
+      htmlElement.classList.remove("dark-mode");
+      setColors(lightColors, animate);
+      togglePressed = "false";
+    }
+    setIconVisibility(dark);
+    if (typeof toggleEl !== "undefined") {
+      toggleEl.forEach(function (element) {
+        element.setAttribute("aria-pressed", togglePressed);
+      });
+    }
+  }
+
+  function checkPreference(e) {
+      const isDarkMode = e.matches; // e.matches will be true if the preference is for dark mode
+      goDark(isDarkMode, false);
+  }
+  
+  const colorPreference = window.matchMedia("(prefers-color-scheme: dark)");
+  colorPreference.addEventListener("change", (e) => {
+    checkPreference(e);
+  });
+
+  let storagePreference = localStorage.getItem("dark-mode");
+  if (storagePreference !== null) {
+    storagePreference === "true" ? goDark(true, false) : goDark(false, false);
+  } else {
+    checkPreference(colorPreference);
+  }
+
+  window.addEventListener("DOMContentLoaded", (event) => {
+    toggleEl = document.querySelectorAll("[tr-color-toggle]");
+    toggleEl.forEach(function (element) {
+      element.setAttribute("aria-label", "View Dark Mode");
+      element.setAttribute("role", "button");
+      element.setAttribute("aria-pressed", togglePressed);
+    });
+    toggleEl.forEach(function (element) {
+      element.addEventListener("click", function () {
+        let darkClass = htmlElement.classList.contains("dark-mode");
+        darkClass ? goDark(false, true) : goDark(true, true);
+      });
+    });
+  });
+}
+colorModeToggle();
+
+/**
+ * ECHARTS
+*/
+
+var dom = document.getElementById('skills-chart');
+var myChart = echarts.init(dom, null, {
+  renderer: 'canvas',
+  useDirtyRect: false
+});
+var app = {};
+
+var option;
+
+option = {
+  backgroundColor: 'transparent',
+  title: {
+    left: 'center',
+    top: 20,
+    textStyle: {
+      color: '#333'
+    }
+  },
+  tooltip: {
+    trigger: 'item'
+  },
+  visualMap: {
+    show: false,
+    min: 80,
+    max: 1000,
+    inRange: {
+      colorLightness: [0, 1]
+    }
+  },
+  series: [
+    {
+      name: 'Access From',
+      type: 'pie',
+      radius: '75%',
+      center: ['50%', '50%'],
+      data: [
+        { value: 700, name: 'UI/UX Design' },
+        { value: 610, name: 'Experience Architecture' },
+        { value: 574, name: 'Product Strategy' },
+        { value: 435, name: 'Content Creation' },
+        { value: 300, name: 'UX Research' },
+        { value: 200, name: 'Web Development' },
+        { value: 100, name: 'Brand Design' }
+      ].sort(function (a, b) {
+        return a.value - b.value;
+      }),
+      roseType: 'radius',
+      label: {
+        color: '#404349'
+      },
+      labelLine: {
+        lineStyle: {
+          color: '#404349'
+        },
+        smooth: 0.2,
+        length: 10,
+        length2: 20
+      },
+      itemStyle: {
+        color: '#c23531',
+        shadowBlur: 200,
+        shadowColor: 'rgba(0, 0, 0, 0.5)'
+      },
+      animationType: 'scale',
+      animationEasing: 'elasticOut',
+      animationDelay: function (idx) {
+        return Math.random() * 200;
+      }
+    }
+  ]
+};
+
+if (option && typeof option === 'object') {
+  myChart.setOption(option);
+}
+
+window.addEventListener('resize', myChart.resize);
