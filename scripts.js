3410c126ef9feb1b9a46301449d28df80a09c3d2
brihol <brianhollett@gmail.com>
2024-06-20 08:29:09 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -13,210 +13,207 @@
 */
 
 function colorModeToggle() {
-  function attr(defaultVal, attrVal) {
-    const defaultValType = typeof defaultVal;
-    if (typeof attrVal !== "string" || attrVal.trim() === "") return defaultVal;
-    if (attrVal === "true" && defaultValType === "boolean") return true;
-    if (attrVal === "false" && defaultValType === "boolean") return false;
-    if (isNaN(attrVal) && defaultValType === "string") return attrVal;
-    if (!isNaN(attrVal) && defaultValType === "number") return +attrVal;
-    return defaultVal;
-  }
-
-  const htmlElement = document.documentElement;
-  const computed = getComputedStyle(htmlElement);
-  let toggleEl;
-  let togglePressed = "false";
-  const darkIcon = document.querySelector(".dark-icon");
-  const liteIcon = document.querySelector(".lite-icon");
-
-  function setIconVisibility(isDarkMode) {
-    darkIcon.style.display = isDarkMode ? "block" : "none";
-    liteIcon.style.display = isDarkMode ? "none" : "block";
-  }
-
-  const scriptTag = document.querySelector("[tr-color-vars]");
-  if (!scriptTag) {
-    console.warn("Script tag with tr-color-vars attribute not found");
-    return;
-  }
-
-  let colorModeDuration = attr(0.5, scriptTag.getAttribute("duration"));
-  let colorModeEase = attr("power1.out", scriptTag.getAttribute("ease"));
-
-  const cssVariables = scriptTag.getAttribute("tr-color-vars");
-  if (!cssVariables.length) {
-    console.warn("Value of tr-color-vars attribute not found");
-    return;
-  }
-
-  let lightColors = {};
-  let darkColors = {};
-  cssVariables.split(",").forEach(function (item) {
-    let lightValue = computed.getPropertyValue(`--color--${item}`);
-    let darkValue = computed.getPropertyValue(`--dark--${item}`);
-    if (lightValue.length) {
-      if (!darkValue.length) darkValue = lightValue;
-      lightColors[`--color--${item}`] = lightValue;
-      darkColors[`--color--${item}`] = darkValue;
+    function attr(defaultVal, attrVal) {
+        const defaultValType = typeof defaultVal;
+        if (typeof attrVal !== "string" || attrVal.trim() === "") return defaultVal;
+        if (attrVal === "true" && defaultValType === "boolean") return true;
+        if (attrVal === "false" && defaultValType === "boolean") return false;
+        if (isNaN(attrVal) && defaultValType === "string") return attrVal;
+        if (!isNaN(attrVal) && defaultValType === "number") return +attrVal;
+        return defaultVal;
     }
-  });
 
-  if (!Object.keys(lightColors).length) {
-    console.warn("No variables found matching tr-color-vars attribute value");
-    return;
-  }
+    const htmlElement = document.documentElement;
+    const computed = getComputedStyle(htmlElement);
+    let toggleEl;
+    let togglePressed = "false";
+    const darkIcon = document.querySelector(".dark-icon");
+    const liteIcon = document.querySelector(".lite-icon");
 
-  function setColors(colorObject, animate) {
-    if (typeof gsap !== "undefined" && animate) {
-      gsap.to(htmlElement, {
-        ...colorObject,
-        duration: colorModeDuration,
-        ease: colorModeEase
-      });
-    } else {
-      Object.keys(colorObject).forEach(function (key) {
-        htmlElement.style.setProperty(key, colorObject[key]);
-      });
+    function setIconVisibility(isDarkMode) {
+        darkIcon.style.display = isDarkMode ? "block" : "none";
+        liteIcon.style.display = isDarkMode ? "none" : "block";
     }
-  }
 
-  function goDark(dark, animate) {
-    const isDarkMode = dark;
-    if (dark) {
-      localStorage.setItem("dark-mode", "true");
-      htmlElement.classList.add("dark-mode");
-      setColors(darkColors, animate);
-      togglePressed = "true";
-    } else {
-      localStorage.setItem("dark-mode", "false");
-      htmlElement.classList.remove("dark-mode");
-      setColors(lightColors, animate);
-      togglePressed = "false";
+    const scriptTag = document.querySelector("[tr-color-vars]");
+    if (!scriptTag) {
+        console.warn("Script tag with tr-color-vars attribute not found");
+        return;
     }
-    setIconVisibility(dark);
-    if (typeof toggleEl !== "undefined") {
-      toggleEl.forEach(function (element) {
-        element.setAttribute("aria-pressed", togglePressed);
-      });
+
+    let colorModeDuration = attr(0.5, scriptTag.getAttribute("duration"));
+    let colorModeEase = attr("power1.out", scriptTag.getAttribute("ease"));
+
+    const cssVariables = scriptTag.getAttribute("tr-color-vars");
+    if (!cssVariables.length) {
+        console.warn("Value of tr-color-vars attribute not found");
+        return;
     }
-  }
 
-  function checkPreference(e) {
-      const isDarkMode = e.matches; // e.matches will be true if the preference is for dark mode
-      goDark(isDarkMode, false);
-  }
-  
-  const colorPreference = window.matchMedia("(prefers-color-scheme: dark)");
-  colorPreference.addEventListener("change", (e) => {
-    checkPreference(e);
-  });
-
-  let storagePreference = localStorage.getItem("dark-mode");
-  if (storagePreference !== null) {
-    storagePreference === "true" ? goDark(true, false) : goDark(false, false);
-  } else {
-    checkPreference(colorPreference);
-  }
-
-  window.addEventListener("DOMContentLoaded", (event) => {
-    toggleEl = document.querySelectorAll("[tr-color-toggle]");
-    toggleEl.forEach(function (element) {
-      element.setAttribute("aria-label", "View Dark Mode");
-      element.setAttribute("role", "button");
-      element.setAttribute("aria-pressed", togglePressed);
+    let lightColors = {};
+    let darkColors = {};
+    cssVariables.split(",").forEach(function (item) {
+        let lightValue = computed.getPropertyValue(`--color--${item}`);
+        let darkValue = computed.getPropertyValue(`--dark--${item}`);
+        if (lightValue.length) {
+            if (!darkValue.length) darkValue = lightValue;
+            lightColors[`--color--${item}`] = lightValue;
+            darkColors[`--color--${item}`] = darkValue;
+        }
     });
-    toggleEl.forEach(function (element) {
-      element.addEventListener("click", function () {
-        let darkClass = htmlElement.classList.contains("dark-mode");
-        darkClass ? goDark(false, true) : goDark(true, true);
-      });
+
+    if (!Object.keys(lightColors).length) {
+        console.warn("No variables found matching tr-color-vars attribute value");
+        return;
+    }
+
+    function setColors(colorObject, animate) {
+        if (typeof gsap !== "undefined" && animate) {
+            gsap.to(htmlElement, {
+                ...colorObject,
+                duration: colorModeDuration,
+                ease: colorModeEase
+            });
+        } else {
+            Object.keys(colorObject).forEach(function (key) {
+                htmlElement.style.setProperty(key, colorObject[key]);
+            });
+        }
+    }
+
+    function updateChartColors(isDarkMode) {
+        const textColor = getComputedStyle(document.documentElement)
+            .getPropertyValue(isDarkMode ? '--dark--text' : '--color--text').trim(); // Assuming you have these CSS variables for text colors
+
+        var option = {
+          backgroundColor: 'transparent',
+          title: {
+            left: 'center',
+            top: 20,
+            textStyle: {
+              color: textColor
+            }
+          },
+          tooltip: {
+            trigger: 'item'
+          },
+          visualMap: {
+            show: false,
+            min: 80,
+            max: 1000,
+            inRange: {
+              colorLightness: [0, 1]
+            }
+          },
+          series: [
+            {
+              name: 'Access From',
+              type: 'pie',
+              radius: '75%',
+              center: ['50%', '50%'],
+              data: [
+                { value: 700, name: 'UI/UX Design' },
+                { value: 610, name: 'Experience Architecture' },
+                { value: 574, name: 'Product Strategy' },
+                { value: 435, name: 'Content Creation' },
+                { value: 300, name: 'UX Research' },
+                { value: 200, name: 'Web Development' },
+                { value: 100, name: 'DesignOps' }
+              ].sort(function (a, b) {
+                return a.value - b.value;
+              }),
+              roseType: 'radius',
+              label: {
+                color: textColor
+              },
+              labelLine: {
+                lineStyle: {
+                  color: textColor
+                },
+                smooth: 0.2,
+                length: 10,
+                length2: 20
+              },
+              itemStyle: {
+                color: '#c23531',
+                shadowBlur: 0,
+                shadowColor: 'rgba(0, 0, 0, 0)'
+              },
+              animationType: 'scale',
+              animationEasing: 'elasticOut',
+              animationDelay: function (idx) {
+                return Math.random() * 200;
+              }
+            }
+          ]
+        };
+        myChart.setOption(option, true); // Update the chart options
+    }
+
+    function goDark(dark, animate) {
+        const isDarkMode = dark; // Assuming 'dark' is a boolean
+        if (isDarkMode) {
+            localStorage.setItem("dark-mode", "true");
+            htmlElement.classList.add("dark-mode");
+            setColors(darkColors, animate);
+            togglePressed = "true";
+        } else {
+            localStorage.setItem("dark-mode", "false");
+            htmlElement.classList.remove("dark-mode");
+            setColors(lightColors, animate);
+            togglePressed = "false";
+        }
+        setIconVisibility(isDarkMode);
+        updateChartColors(isDarkMode); // Call this function to update chart colors
+        if (typeof toggleEl !== "undefined") {
+            toggleEl.forEach(function (element) {
+                element.setAttribute("aria-pressed", togglePressed);
+            });
+        }
+    }
+
+    function checkPreference(e) {
+        goDark(e.matches, false);
+    }
+
+    const colorPreference = window.matchMedia("(prefers-color-scheme: dark)");
+    colorPreference.addEventListener("change", (e) => {
+        checkPreference(e);
     });
-  });
+
+    let storagePreference = localStorage.getItem("dark-mode");
+    if (storagePreference !== null) {
+        storagePreference === "true" ? goDark(true, false) : goDark(false, false);
+    } else {
+        checkPreference(colorPreference);
+    }
+
+    window.addEventListener("DOMContentLoaded", (event) => {
+        toggleEl = document.querySelectorAll("[tr-color-toggle]");
+        toggleEl.forEach(function (element) {
+            element.setAttribute("aria-label", "View Dark Mode");
+            element.setAttribute("role", "button");
+            element.setAttribute("aria-pressed", togglePressed);
+        });
+        toggleEl.forEach(function (element) {
+            element.addEventListener("click", function () {
+                let darkClass = htmlElement.classList.contains("dark-mode");
+                darkClass ? goDark(false, true) : goDark(true, true);
+            });
+        });
+
+        // Initialize ECharts
+        var dom = document.getElementById('skills-chart');
+        window.myChart = echarts.init(dom, null, {
+            renderer: 'canvas',
+            useDirtyRect: false
+        });
+        updateChartColors(document.documentElement.classList.contains("dark-mode"));
+        window.addEventListener('resize', function () {
+            myChart.resize();
+        });
+    });
 }
+
 colorModeToggle();
 
-/**
- * ECHARTS
-*/
-
-var dom = document.getElementById('skills-chart');
-// Get Theme Color
-const textColor = getComputedStyle(document.documentElement).getPropertyValue(isDarkMode ? '--dark--text' : '--color--text').trim();
-
-var myChart = echarts.init(dom, null, {
-  renderer: 'canvas',
-  useDirtyRect: false
-});
-var app = {};
-
-var option;
-
-option = {
-  backgroundColor: 'transparent',
-  title: {
-    left: 'center',
-    top: 20,
-    textStyle: {
-      color: textColor
-    }
-  },
-  tooltip: {
-    trigger: 'item'
-  },
-  visualMap: {
-    show: false,
-    min: 80,
-    max: 1000,
-    inRange: {
-      colorLightness: [0, 1]
-    }
-  },
-  series: [
-    {
-      name: 'Access From',
-      type: 'pie',
-      radius: '75%',
-      center: ['50%', '50%'],
-      data: [
-        { value: 700, name: 'UI/UX Design' },
-        { value: 610, name: 'Experience Architecture' },
-        { value: 574, name: 'Product Strategy' },
-        { value: 435, name: 'Content Creation' },
-        { value: 300, name: 'UX Research' },
-        { value: 200, name: 'Web Development' },
-        { value: 100, name: 'DesignOps' }
-      ].sort(function (a, b) {
-        return a.value - b.value;
-      }),
-      roseType: 'radius',
-      label: {
-        color: textColor
-      },
-      labelLine: {
-        lineStyle: {
-          color: textColor
-        },
-        smooth: 0.2,
-        length: 10,
-        length2: 20
-      },
-      itemStyle: {
-        color: '#c23531',
-        shadowBlur: 0,
-        shadowColor: 'rgba(0, 0, 0, 0)'
-      },
-      animationType: 'scale',
-      animationEasing: 'elasticOut',
-      animationDelay: function (idx) {
-        return Math.random() * 200;
-      }
-    }
-  ]
-};
-
-if (option && typeof option === 'object') {
-  myChart.setOption(option);
-}
-
-window.addEventListener('resize', myChart.resize);
